f0e671c537ebed6d7dcb91d89df08ae4865d0f70
Moises Lopez <mdlopezme@gmail.com>
2025-02-21 15:55:40 -1000

    Update brand name from "mrva" to "ava" in CarInterface

--- opendbc/car/ava/interface.py
+++ opendbc/car/ava/interface.py
@@ -5,7 +5,7 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
-    ret.brand = "mrva"
+    ret.brand = "ava"
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.ava)]
 
     # Needs safety validation and final testing before pulling out of dashcam
