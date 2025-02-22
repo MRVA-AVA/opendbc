cc916104f16cef3a862804d185e863b2221155d0
Moises Lopez <mdlopezme@gmail.com>
2025-02-21 21:29:10 -1000

    Add initial implementation for Ava platform and fingerprints

---
+++ opendbc/car/ava/values.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from opendbc.car import Platforms, PlatformConfig, DbcDict, Bus, CarSpecs
+from opendbc.car.structs import CarState
+from enum import IntEnum
+
+@dataclass
+class AvaCarDocs:
+  package: str = "All"
+
+@dataclass
+class AvaPlatformConfig(PlatformConfig):
+  dbc_dict: DbcDict = field(default_factory=lambda: {Bus.pt: 'ava_pt'})
+
+class DriveMode(IntEnum):
+  FORWARD_DRIVE = 1
+  REVERSE_DRIVE = 2
+  PARK = 3
+  NEUTRAL = 4
+
+class CAR(Platforms):
+  AVA_PT = AvaPlatformConfig(
+    [AvaCarDocs("Ava PT")],
+    CarSpecs(mass=818., wheelbase=2.4, steerRatio=12.0),
+  )
+
+class CANBUS:
+  vehicle = 0
+
+DBC = CAR.create_dbc_map()
